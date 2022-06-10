f460e1a736e2fd290277f0e3a110b39b2479c283
Matthias Specht <sylence@btbsoft.org>
2022-06-10 15:12:44 +0200

    Add info links

---
+++ VisualDbml/ViewModels/Commands.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace VisualDbml.ViewModels;
+
+public static class Commands
+{
+	public static ICommand OpenUrlCommand { get; } = ReactiveCommand.Create<string>(OpenUrl);
+
+	private static void OpenUrl(string url)
+	{
+		if (!url.StartsWith("http"))
+			url = "https://" + url;
+
+		Process.Start(new ProcessStartInfo(url)
+		{
+			UseShellExecute = true
+		});
+	}
+}
